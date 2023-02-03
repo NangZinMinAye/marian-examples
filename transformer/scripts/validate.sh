e6632b0a589185ce6635625963bf0d0cb536e9e7
Nang Zin <95068752+NangZinMinAye@users.noreply.github.com>
2023-02-03 09:54:01 +0630

    Update validate.sh

--- transformer/scripts/validate.sh
+++ transformer/scripts/validate.sh
@@ -3,6 +3,6 @@
 cat $1 \
     | sed 's/\@\@ //g' \
     | ../tools/moses-scripts/scripts/recaser/detruecase.perl 2>/dev/null \
-    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l de 2>/dev/null \
-    | ../tools/moses-scripts/scripts/generic/multi-bleu-detok.perl data/valid.de \
+    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l my 2>/dev/null \
+    | ../tools/moses-scripts/scripts/generic/multi-bleu-detok.perl data/valid.my \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
